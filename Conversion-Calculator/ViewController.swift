c88e6fc1729cf8690b26cb58d4c6834a1d3dcb03
Cole Sellers <colesellers0@gmail.com>
2019-09-20 12:45:43 -0400

    yards->meters and vise versa work

--- Conversion-Calculator/ViewController.swift
+++ Conversion-Calculator/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fromInput: DecimalMinusTextField!
+    @IBOutlet weak var toInput: DecimalMinusTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculate(_ sender: UIButton) {
+        if(self.fromInput.text! != ""){
+            let value = Double(self.fromInput.text!)
+            self.toInput.text = String(0.9144 / value!)
+        } else {
+            let value = Double(self.toInput.text!)
+            self.fromInput.text = String (value! * 1.0936)
+        }
+    }
+    
+    @IBAction func clearBtn(_ sender: Any) {
+        self.fromInput.text = ""
+        self.toInput.text = ""
+    }
 }
 
