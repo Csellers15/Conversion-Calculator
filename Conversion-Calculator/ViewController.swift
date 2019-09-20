7c2b2b2d57c8b5ba0d2fda9b8d580efd49dd4635
Cole Sellers <colesellers0@gmail.com>
2019-09-20 13:25:20 -0400

    Everything for part A is done except for removing focus

--- Conversion-Calculator/ViewController.swift
+++ Conversion-Calculator/ViewController.swift
@@ -18,15 +18,26 @@ class ViewController: UIViewController {
         self.fromInput.clearsOnBeginEditing = true
         self.toInput.clearsOnBeginEditing = true
     }
-
+    
     @IBAction func calculate(_ sender: UIButton) {
         if(self.fromInput.text! != ""){
             let value = Double(self.fromInput.text!)
             self.toInput.text = String(0.9144 / value!)
-        } else {
+        } else if(self.toInput.text! != ""){
             let value = Double(self.toInput.text!)
             self.fromInput.text = String (value! * 1.0936)
+        } else {
+            self.fromInput.text = String(0)
+            self.toInput.text = String(0)
         }
+    }
+    
+    @IBAction func fromBeganEditing(_ sender: Any) {
+        self.toInput.text = ""
+    }
+    
+    @IBAction func tobeganEditing(_ sender: Any) {
+        self.fromInput.text = ""
     }
     
     @IBAction func clearBtn(_ sender: Any) {
